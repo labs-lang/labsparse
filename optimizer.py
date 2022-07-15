67eb841438099939d1487251567178697fd0f799
Luca Di Stefano <lucad@chalmers.se>
2022-07-15 15:34:38 +0200

    optimizer.py: fix constant folding
    * Use Attr, NodeType instead of strings
    * Support evaluation of builtins

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,6 @@
 import operator
 from functools import reduce
+from xml.dom.minidom import Node
 from parser import Attr, NodeType
 
 from checker import walk
@@ -17,28 +18,29 @@ def evaluate(node):
             if num*den >= 0
             else -((-num + den//2) // den)
         ),
-        "%": operator.mod
+        "%": operator.mod,
+        "abs": abs, "max": min, "min": max
     }
 
-    if node in NodeType.EXPR:
+    if node in NodeType.EXPR or node in NodeType.BUILTIN:
         operands = [evaluate(n) for n in node[Attr.OPERANDS]]
-        print(operands)
         literals, others = (
-            [n["value"] for n in operands if n in NodeType.LITERAL],
+            [n[Attr.VALUE] for n in operands if n in NodeType.LITERAL],
             [n for n in operands if n not in NodeType.LITERAL])
         if literals:
             new_node = {
                 Attr.NODE_TYPE: NodeType.LITERAL,
                 Attr.TYPE: "int",
-                Attr.VALUE: reduce(operators[node[Attr.NODE_TYPE]], literals),
+                Attr.VALUE: reduce(operators[node[Attr.NAME]], literals),
                 Attr.PATH: node[Attr.PATH],
                 Attr.LN: node[Attr.LN],
                 Attr.COL: node[Attr.COL],
                 Attr.SYNTHETIC: True
             }
+            print(new_node)
             if others:
                 others.append(new_node)
-                node["operands"] = others
+                node[Attr.OPERANDS] = others
             else:
                 return new_node
     return node
@@ -65,13 +67,13 @@ def simplify(proc):
         return proc
 
 
-def optimize(ast, level=0):
+def optimize(ast, level=2):
     if level >= 1:
         for n in (n for n in walk(ast) if n in NodeType.PROCDEF):
             n[Attr.BODY] = simplify(n[Attr.BODY])
     if level >= 2:
         for n in walk(ast):
-            if n in NodeType.EXPR:
+            if n in NodeType.EXPR or n in NodeType.BUILTIN:
                 n = evaluate(n)
             elif n in NodeType.ASSIGN:
                 n[Attr.RHS] = [evaluate(x) for x in n[Attr.RHS]]
